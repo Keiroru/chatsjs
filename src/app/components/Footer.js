6df445e6f103ed99335f89a6679b2bb324c29665
NoeŁ <124210421+Keiroru@users.noreply.github.com>
2025-04-14 10:14:30 +0200

    footer styling + js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,17 +1,22 @@
-import Link from 'next/link';
-import styles from '@/app/styles/Footer.module.css';
+import React from "react";
+import styles from "@/app/styles/Footer.module.css";
 
-export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.container}>
-                <Link href="/tos" className={styles.link}>
-                    Terms of Service
-                </Link>
-                <Link href="/privacy-policy" className={styles.link}>
-                    Privacy Policy
-                </Link>
-            </div>
-        </footer>
-    );
-} 
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>&copy; {new Date().getFullYear()} ChatJS. All rights reserved.</p>
+        <div className={styles.footerLinks}>
+          <a href="/tos" className={styles.link}>
+            Terms of Service
+          </a>
+          <a href="/privacy-policy" className={styles.link}>
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
